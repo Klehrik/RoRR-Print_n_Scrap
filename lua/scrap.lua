515c8501003d18934e11304c4bed5a4395b794d1
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-12-07 14:00:10 -0500

    Update scrap.lua

--- lua/scrap.lua
+++ lua/scrap.lua
@@ -1,7 +1,11 @@
 -- Scrap (Items)
 
-local sScrapWhite   = Resources.sprite_load("printNScrap", "scrapWhite", PATH.."sprites/scrapWhite.png", 1, 13, 13)
-local sScrapGreen   = Resources.sprite_load("printNScrap", "scrapGreen", PATH.."sprites/scrapGreen.png", 1, 13, 13)
-local sScrapRed     = Resources.sprite_load("printNScrap", "scrapRed", PATH.."sprites/scrapRed.png", 1, 13, 13)
-local sScrapYellow  = Resources.sprite_load("printNScrap", "scrapYellow", PATH.."sprites/scrapYellow.png", 1, 13, 13)
+local names = {"White", "Green", "Red", "Yellow"}
+local tiers = {Item.TIER.common, Item.TIER.uncommon, Item.TIER.rare, Item.TIER.boss}
 
+for i = 1, 4 do
+    local item = Item.new("printNScrap", "scrap"..names[i], true)
+    item:set_sprite(Resources.sprite_load("printNScrap", "scrap"..names[i], PATH.."sprites/scrap"..names[i]..".png", 1, 13, 13))
+    item:set_tier(tiers[i])
+    item:toggle_loot(false)
+end
